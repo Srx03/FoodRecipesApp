abcc0c6c69f3973810a8c9a80d2e5a1f1aea375c
Srdjan Djuric <srdjan.djuric003@gmail.com>
2022-11-03 18:41:20 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const randomMealImg = document.getElementById('img-random');
 const randomMealName = document.getElementById('random-name');
 const listElement = document.querySelector("#seafoods");
+const mealDetailsContent = document.querySelector('.meal-details-content');
 
 
 
@@ -29,4 +30,36 @@ const listElement = document.querySelector("#seafoods");
        });
 
     })
+
     
+
+    function getMealRecipe(e){
+        e.preventDefault();
+        if(e.target.classList.contains('recipe-btn')){
+            let mealItem = e.target.parentElement.parentElement;
+            fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+            .then(response => response.json())
+            .then(data => mealRecipeModal(data.meals));
+        }
+    }
+
+    function mealRecipeModal(meal){
+        console.log(meal);
+        meal = meal[0];
+        let html = `
+            <h2 class = "recipe-title">${meal.strMeal}</h2>
+            <p class = "recipe-category">${meal.strCategory}</p>
+            <div class = "recipe-instruct">
+                <h3>Instructions:</h3>
+                <p>${meal.strInstructions}</p>
+            </div>
+            <div class = "recipe-meal-img">
+                <img src = "${meal.strMealThumb}" alt = "">
+            </div>
+            <div class = "recipe-link">
+                <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+            </div>
+        `;
+        mealDetailsContent.innerHTML = html;
+        mealDetailsContent.parentElement.classList.add('showRecipe');
+    }
